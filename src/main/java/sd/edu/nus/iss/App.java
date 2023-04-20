ce2b16e54c7d62d811de54afe4a87d550ea0e78e
niggchua <niggchua@gmail.com>
2023-04-20 11:00:50 +0800

    first commit

---
+++ src/main/java/sd/edu/nus/iss/App.java
@@ -0,0 +1,13 @@
+package sd.edu.nus.iss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/sd/edu/nus/iss/AppTest.java
@@ -0,0 +1,20 @@
+package sd.edu.nus.iss;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
